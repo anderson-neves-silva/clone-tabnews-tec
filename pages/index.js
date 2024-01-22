231c6f737ce233d5fa16b3b6ad8cc63554a8c2c5
Anderson Neves Da Silva <andersonneves_@hotmail.com>
2024-01-22 19:11:08 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      Olá Juju sabe quem é ? Kkk 🤓 mãe hoje vim aqui agradecer por você fazer
+      parte da minha vida, saiba que amo muito você, se você me ama assobie três
+      vezes.
+    </h1>
+  );
+}
+
+export default Home;
