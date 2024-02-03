3a2f2e22f7ea38325c39e7b38cd13e167a021aa2
Anderson Neves Da Silva <andersonneves_@hotmail.com>
2024-02-03 13:08:47 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <h1>
-      Olá Juju sabe quem é ? Kkk 🤓 mãe hoje vim aqui agradecer por você fazer
-      parte da minha vida, saiba que amo muito você, se você me ama assobie três
-      vezes.
+      Olá Juju sabe quem é? mãe hoje vim aqui agradecer por você fazer parte da
+      minha vida, saiba que amo muito você, se você me ama assobie três vezes
+      kkk 🤓.
     </h1>
   );
 }
