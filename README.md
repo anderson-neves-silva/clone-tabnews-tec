6d6ae2687c59f0a9db6e8cd879c9721fec3d676d
Anderson Neves Da Silva <andersonneves_@hotmail.com>
2024-03-03 17:24:34 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews-tec
+
 Projeto criado para aprender programação web, esses conteúdos é para quem trabalha com Programação e Tecnologia.
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 function Home() {
-  return (
-    <h1>
-      Olá Juju sabe quem é? mãe hoje vim aqui agradecer por você fazer parte da
-      minha vida, saiba que amo muito você, se você me ama assobie três vezes
-      kkk 🤓.
-    </h1>
-  );
+  return;
+  <h1>
+    Olá Juju sabe quem é? mãe hoje vim aqui agradecer por você fazer parte da
+    minha vida, saiba que amo muito você, se você me ama assobie três vezes kkk
+    🤓.
+  </h1>;
 }
 
 export default Home;
